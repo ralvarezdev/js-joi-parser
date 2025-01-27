636315f225d4694ecf149e2a4aaae6460572ca20
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-27 15:47:06 -0400

    fix: solved undefined validatedBody bug

--- parser/validate.js
+++ parser/validate.js
@@ -3,7 +3,7 @@ import ParseError from "./parser.js";
 // Validate and parse the request body using Joi
 export default function Validate(body, schema, abortEarly = false){
     // Validate the request body
-    const {error, validatedBody} = schema.validate(body, { abortEarly });
+    const {error, value: validatedBody} = schema.validate(body, { abortEarly });
 
     // Check if there is an error
     if (error) return [ParseError(error), false]
